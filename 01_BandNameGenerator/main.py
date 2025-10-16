3e12efa237f8e0bf8ba34a7c8884d032c428d03d
Prajwal B <98519010+praj176@users.noreply.github.com>
2025-10-16 19:52:45 +0530

    Code of day 1

--- 01_BandNameGenerator/main.py
+++ 01_BandNameGenerator/main.py
@@ -1,1 +1,6 @@
+print("Welcome to Pathak Name Generator")
 
+city = input("Please enter the name of your city :\n")
+animal = input("Please enter the animal you would like to name: \n")
+
+print("The name of your pathak is " + city + " " + animal + "s")
